8c492171fee6164028725361b1a052e216545c7c
momo <rhwoah7190@gmail.com>
2023-09-07 21:55:52 +0900

    MiniProject(개인) 09/07

--- 03_JDBC/jdbc_project/src/edu/kh/jdbc/model/dao/MiniProjectDAO.java
+++ 03_JDBC/jdbc_project/src/edu/kh/jdbc/model/dao/MiniProjectDAO.java
@@ -1,5 +1,38 @@
 package edu.kh.jdbc.model.dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+import edu.kh.jdbc.model.dto.MiniProject;
+
 public class MiniProjectDAO {
+
+	private Statement stmt = null;
+	private PreparedStatement pstmt = null;
+	private ResultSet rs = null;
+	
+	public int insertMember(Connection conn, MiniProject member) {
+		int result = 0;
+		
+		String sql = "INSERT INTO MAINMEMBER\n"
+				+ "VALUES(SEQ_MAIN_MEMBER_NO,?,?,?,?,?,?,DEFAULT)";
+		
+		try {
+			pstmt = conn.prepareStatement(sql);
+			
+			pstmt.setString(1, member.getMemberId());
+			pstmt.setString(2, member.getMemberPw());
+			pstmt.setString(1, member.getPhoneNumber());
+			pstmt.setString(1, member.getMemberAddress());
+			pstmt.setString(1, member.getMemberId());
+			pstmt.setString(1, member.getMemberId());
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return 0;
+	}
 
 }
--- 03_JDBC/jdbc_project/src/edu/kh/jdbc/model/service/MiniProjectService.java
+++ 03_JDBC/jdbc_project/src/edu/kh/jdbc/model/service/MiniProjectService.java
@@ -2,16 +2,24 @@ package edu.kh.jdbc.model.service;
 
 import java.sql.Connection;
 
+
 import edu.kh.jdbc.model.dao.MiniProjectDAO;
 import edu.kh.jdbc.model.dto.MiniProject;
-
+import static edu.kh.jdbc.common.JDBCTemplate.*;
 public class MiniProjectService {
 
 	private MiniProjectDAO dao = new MiniProjectDAO();
 	
 	public int insertMember(MiniProject member) {
-		Connection conn = 
-		return 0;
+		Connection conn = getConnection();
+		
+		int result = dao.insertMember(conn, member);
+		
+		if(result > 0) commit(conn);
+		else		rollback(conn);
+		
+		close(conn);
+		return result;
 	}
 
 }
