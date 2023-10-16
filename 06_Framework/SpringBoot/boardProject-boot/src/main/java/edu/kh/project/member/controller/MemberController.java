6399c7c725a8b4f761229a1f2e81bb037d260873
MOMO <rhwoah7190@gmail.com>
2023-10-16 17:45:28 +0900

    10/16 String boot

---
+++ 06_Framework/SpringBoot/boardProject-boot/src/main/java/edu/kh/project/member/controller/MemberController.java
@@ -0,0 +1,49 @@
+package edu.kh.project.member.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import edu.kh.project.member.model.dto.Member;
+import edu.kh.project.member.model.service.MemberService;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j // 로그 찍어보기
+@Controller // Controller 역할(응답/요청 제어) + bean 등록
+@RequestMapping("/member")
+@SessionAttributes({"loginMember"}) // session 올리기
+public class MemberController {
+
+	// MemberService 의존성주입(DI)
+	@Autowired
+	private MemberService service;
+	
+	/**
+	 * @param inputMember : 아이디, 비밀번호
+	 * @param model : 데이터 전달 객체
+	 * @param ra : 리다이렉트 시 request scope로 데이터 전달
+	 * @return 메인 페이지로 리다이렉트
+	 */
+	@PostMapping("login")
+	public String login(Member inputMember, Model model, RedirectAttributes ra) {
+		
+		// 로그인 서비스 호출 
+		Member loginMember = service.login(inputMember);
+		
+		// 로그인 성공 시
+		if(loginMember != null) {
+			// 쿠키 생성 코드 작성 예정
+		}
+		// 로그인 실패 시
+		if(loginMember == null) {
+			ra.addFlashAttribute("message","아이디 또는 비밀번호가 일치하지 않습니다.");
+		}
+		model.addAttribute("loginMember",loginMember); // 로그인 결과를 session에 등록
+		// 메인페이지로 리다이렉트
+		return "redirect:/";
+	}
+}
