6e7deecc035bb7b10f31b3e1f1ab285d3ad6b2ff
momo <rhwoah7190@gmail.com>
2023-11-05 22:22:17 +0900

    11/2 수업 내용

--- 06_Framework/SpringBoot/boardProject-boot/src/main/java/edu/kh/project/board/controller/EditBoardController.java
+++ 06_Framework/SpringBoot/boardProject-boot/src/main/java/edu/kh/project/board/controller/EditBoardController.java
@@ -1,17 +1,22 @@
 package edu.kh.project.board.controller;
 
+import java.io.IOException;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttribute;
 import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import edu.kh.project.board.model.dto.Board;
 import edu.kh.project.board.model.service.EditBoardService;
 import edu.kh.project.member.model.dto.Member;
 import lombok.RequiredArgsConstructor;
@@ -26,25 +31,30 @@ public class EditBoardController {
 	
 	
 	/** 게시글 삭제
-	 * @param boardCode : 게시판 코드
+	 * @param boardCode : 게시판 구분 코드
 	 * @param boardNo : 게시글 번호(PK)
-	 * @param loginMember 세션에 저장된 로그인된 회원의 정보
+	 * @param loginMember : 세션에 저장된 로그인된 회원의 정보(로그인 X -> null)
 	 * @param ra : 리다이렉트 시 request scope로 데이터 전달
-	 * @param referer : 요청 헤더에 기록된 요청 이전 페이지 주소 (referer == 흔적, 단석)
-	 * @return 
+	 * @param referer : 요청 헤더에 기록된 요청 이전 페이지 주소
+	 *		 (referer == 흔적)
+	 * @return
 	 */
-	@GetMapping("{boardCode:[0-9]+}/{boardNo:[0-9]+}/delete")
-	public String deleteBoard(@PathVariable("boardCode") int boardCode,
-							@PathVariable("boardNo") int boardNo,
-							@SessionAttribute(value="loginMember",required = false) Member loginMember,
-							RedirectAttributes ra
-							/*@RequestHeader("referer") String referer*/) {
 	
+	@GetMapping("{boardCode:[0-9]+}/{boardNo:[0-9]+}/delete")
+	public String deleteBoard(
+		@PathVariable("boardCode") int boardCode,
+		@PathVariable("boardNo") int boardNo,
+		@SessionAttribute(value="loginMember", required=false) Member loginMember, 
+		RedirectAttributes ra
+		//,@RequestHeader("referer") String referer
+			) {
+		
 		// 로그인 상태가 아닌 경우 삭제 진행 X
 		if(loginMember == null) {
-			ra.addFlashAttribute("message","로그인 후 이용해주세요.");
+			ra.addFlashAttribute("message", "로그인 후 이용해주세요");
 			return "redirect:/member/login";
 		}
+		
 		// Map을 이용해서 boardCode, boardNo, memberNo 담기
 		Map<String, Integer> paramMap = new HashMap<>();
 		paramMap.put("boardCode", boardCode);
@@ -57,22 +67,86 @@ public class EditBoardController {
 		String path = null;
 		String message = null;
 		
-		if(result>0) {
-			message = "삭제되었습니다.";
-			path = "redirect:/board/" + boardCode; // 해당 게시판 첫 페이지
+		if(result > 0) {
+			message = "삭제 되었습니다";
+			path = "redirect:/board/"+boardCode; // 해당 게시판 첫 페이지
+			
 		}else {
 			message = "삭제 실패";
-			// 메인 페이지 리다이렉트
+			
+			// 메인 페이지로 리다이렉트
 			path = "redirect:/";
 			
 			// 이전 요청 페이지로 리다이렉트
 			//path = "redirect:" + referer;
 		}
-		ra.addFlashAttribute("message",message);
+		
+		ra.addFlashAttribute("message", message);
 		return path;
 	}
 	
 	
 	
+	/** 게시글 작성 화면 전환
+	 * @return
+	 */
+	@GetMapping("{boardCode:[0-9]+}/insert")
+	public String insertBoard(
+		@PathVariable("boardCode") int boardCode,
+		@SessionAttribute(value="loginMember", required=false) Member loginMember) {
+		
+		if(loginMember == null) {
+			return "redirect:/board/" + boardCode;
+		}
+		
+		return "board/boardWrite";
+	}
+	
+	
+	
+	/** 게시글 작성 (게시글 작성 + 파일 0~5개 업로드)
+	 * @param boardCode : 게시판 코드
+	 * @param loginMember : 로그인한 회원 정보(세션)
+	 * @param board : 파라미터가 담긴 커맨드 객체
+	 * @param images : 제출된 input type="file" name="images" 파라미터 묶음
+	 * 		 (주의!) 제출된 파일이 없어도 input 수 만큼 List 요소 존재
+	 * 
+	 * @param ra : 리다이렉트 시 request scope로 데이터 전달
+	 * @return
+	 * @throws IOException 
+	 * @throws IllegalStateException 
+	 */
+	@PostMapping("{boardCode:[0-9]+}/insert")
+	public String insertBoard(
+		@PathVariable("boardCode") int boardCode,
+		@SessionAttribute("loginMember") Member loginMember,
+		Board board,
+		@RequestParam("images") List<MultipartFile> images,
+		RedirectAttributes ra
+		) throws IllegalStateException, IOException {
+		
+		// 로그인한 회원의 번호, 게시판 코드를 
+		// 커맨드 객체 board에 세팅
+		board.setMemberNo(loginMember.getMemberNo());
+		board.setBoardCode(boardCode);
+		
+		// 서비스 호출 후 결과 반환 받기
+		int boardNo = service.insertBoard(board, images);
+				
+		
+		// 게시글 작성 성공 시
+		// 지금 작성한 글의 상세 조회 화면으로 리다이렉트
+		//  /board/{boardCode}/{boardNo}
+		if(boardNo > 0) {
+			ra.addFlashAttribute("message", "게시글 작성 성공");
+			return 
+				String.format("redirect:/board/%d/%d", boardCode, boardNo);
+		}
+		
+		
+		// 실패 시
+		ra.addFlashAttribute("message", "게시글 작성 실패...");
+		return "redirect:insert";
+	}
 	
 }
--- 06_Framework/SpringBoot/boardProject-boot/src/main/java/edu/kh/project/myPage/controller/MyPageController.java
+++ 06_Framework/SpringBoot/boardProject-boot/src/main/java/edu/kh/project/myPage/controller/MyPageController.java
@@ -3,9 +3,7 @@ package edu.kh.project.myPage.controller;
 import java.io.IOException;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.bcrypt.BCrypt;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,156 +18,201 @@ import edu.kh.project.member.model.dto.Member;
 import edu.kh.project.myPage.model.service.MyPageService;
 import lombok.extern.slf4j.Slf4j;
 
-@Slf4j
 @Controller
 @RequestMapping("myPage")
 @SessionAttributes({"loginMember"})
+@Slf4j
 public class MyPageController {
-
+	
 	@Autowired
 	private MyPageService service;
 	
-	// 마이페이지 화면 전환 (forward)
-	@GetMapping("info") // /myPage/info
+	// 마이 페이지 화면 전환 (forward)
+	
+	@GetMapping("info") //   /myPage/info
 	public String info() {
+		
+		// templates/myPage/myPage-info.html   
 		return "myPage/myPage-info";
 	}
-	@GetMapping("profile") // /myPage/info
+	
+	@GetMapping("profile") 
 	public String profile() {
 		return "myPage/myPage-profile";
 	}
-	@GetMapping("changePw") // /myPage/info
+	
+	@GetMapping("changePw") 
 	public String changePw() {
 		return "myPage/myPage-changePw";
 	}
-	@GetMapping("secession") // /myPage/info
+	
+	@GetMapping("secession") 
 	public String secession() {
 		return "myPage/myPage-secession";
 	}
 	
+
+	// -----------------------------------------------
+	
 	/** 회원 정보 수정
-	 * @param updateMember : 수정된 회원정보가 담긴 커맨드 객체
+	 * @param updateMember : 수정된 회원 정보가 담긴 커맨드 객체
 	 * @param memberAddress : 주소 값이 담긴 배열
 	 * @param loginMember : 현재 로그인한 회원의 정보가 담긴 객체(session)
-	 * @param RedirectAttributes : 리다이렉트 시 requeset scope로 데이터 전달
+	 * @param ra : 리다이렉트 시 request scope로 데이터 전달
 	 * @return
 	 */
 	@PostMapping("info")
-	public String info(Member updateMember, String[] memberAddress, @SessionAttribute("loginMember") Member loginMember, RedirectAttributes ra) {
-		// 1) loginMember에서 회원 번호만 얻어와 updateMember에 세팅
-		updateMember.setMemberNo(loginMember.getMemberNo()); 
+	public String info(Member updateMember, String[] memberAddress,
+		@SessionAttribute("loginMember") Member loginMember,
+		RedirectAttributes ra) {
 		
-		// 2) 회원정보 수정 서비스 호출
+		// 1. loginMember에서 회원 번호만 얻어와 updateMember에 세팅
+		updateMember.setMemberNo( loginMember.getMemberNo() );
+		
+		// 2. 회원 정보 서비스 호출 후 결과 반환 받기
 		int result = service.info(updateMember, memberAddress);
 		
-		// 3) 서비스 처리 결과에 따라 응답 제어
+		// 3. 서비스 처리 결과에 따라 응답 제어
 		
 		String message = null;
-		// 성공 시
-		// - message = "회원 정보가 수정되었습니다."
-		// - session에 세팅된 이전 회원 정보를 수정된 회원 정보로 다시 세팅
+		// [성공]
+		// - message = "회원 정보가 수정 되었습니다"
+		// - session에 세팅된 이전 회원 정보를
+		//   수정된 회원 정보로 다시 세팅
 		if(result > 0) {
-			message = "회원 정보가 수정되었습니다.";
+			message = "회원 정보가 수정 되었습니다";
 			loginMember.setMemberNickname(updateMember.getMemberNickname());
 			loginMember.setMemberTel(updateMember.getMemberTel());
 			loginMember.setMemberAddress(updateMember.getMemberAddress());
-		} else {
-			message = "회원 정보 수정 실패";
 		}
-		// 실패 시
-		// - message = "회원 정보 수정 실패"
 		
-		ra.addFlashAttribute("message",message);
+		// [실패]
+		// - message = "회원 정보 수정 실패..."
+		else {
+			message = "회원 정보가 수정 실패...";
+		}
+		
+		ra.addFlashAttribute("message", message);
+		
 		return "redirect:info";
 	}
 	
-	/**
-	 * @param currentPw : 현재 비밀번호(@RequetsParam 생략)
-	 * @param newPw : 새 비밀번호
-	 * @param loginMember : 로그인한 회원(session)
-	 * @param ra
+	
+	
+	
+	/** 비밀번호 변경
+	 * @param currentPw : 현재 비밀번호 (@RequestParam 생략)
+	 * @param newPw : 새 비밀번호 (@RequestParam 생략)
+	 * @param loginMember : 로그인한 회원 (session)
+	 * @param ra : 리다이렉트 시 데이터 전달
 	 * @return
 	 */
 	@PostMapping("changePw")
-	public String changePw(String currentPw, String newPw ,
-			@SessionAttribute("loginMember") Member loginMember, RedirectAttributes ra) {
+	public String changePw(String currentPw, String newPw,
+		@SessionAttribute("loginMember") Member loginMember,
+		RedirectAttributes ra) {
+
+		// 비밀번호 변경 서비스 호출
+		int result = service.changePw(currentPw, newPw, loginMember.getMemberNo());
 		
-		int memberNo = loginMember.getMemberNo();
-		
-		int result = service.changePw(currentPw, newPw, memberNo);
 		String path = null;
 		String message = null;
 		
-		if(result>0) {
-			message = "비밀번호 변경 성공";
+		if(result > 0) {
+			message = "비밀번호가 변경 되었습니다";
 			path = "redirect:info";
+			
 		}else {
-			message = "비밀번호가 일치하지 않습니다.";
+			message = "현재 비밀번호가 일치하지 않습니다";
 			path = "redirect:changePw";
 		}
-		ra.addFlashAttribute("message",message);
-
-		return path;
-	}
-	
-	@PostMapping("secession")
-	public String secession(String memberPw,@SessionAttribute("loginMember") Member loginMember, RedirectAttributes ra, SessionStatus status) {
 		
-		int memberNo = loginMember.getMemberNo();
-		
-		int result = service.secession(memberPw, memberNo);
-		
-		String path = null;
-		String message = null;
-		if(result>0) {
-			message = "탈퇴 성공";
-			status.setComplete();
-			path = "redirect:/";
-		}else {
-			message = "비밀번호가 일치하지 않습니다.";
-			path = "redirect:/secession";
-		}
-	
 		ra.addFlashAttribute("message", message);
 		return path;
 	}
 	
-	// MultipartFIle : multipart/form-data 형식으로 제출된 파라미터 중 file 타입 데이터만 
-	//					추출해 저장하는 Spring 제공 객체
+	
+	/** 회원 탈퇴
+	 * @param memberPw : 입력 받은 비밀번호
+	 * @param loginMemebr : 로그인한 회원 정보 (session)
+	 * @param ra : 리다이렉트 시 데이터 전달
+	 * @param status : 세션 관리 객체
+	 * @return
+	 */
+	@PostMapping("secession")
+	public String secession(String memberPw, 
+		@SessionAttribute("loginMember") Member loginMember,
+		RedirectAttributes ra,
+		SessionStatus status) {
+		
+		int result = service.secession(memberPw, loginMember.getMemberNo());
+		
+		String message = null;
+		String path = null;
+		
+		if(result > 0) { // 탈퇴 O
+			message = "탈퇴 되었습니다";
+			path = "redirect:/";
+			
+			status.setComplete(); // 세션 만료
+		}else { // 탈퇴 X
+			message = "비밀번호가 일치하지 않습니다";
+			path = "redirect:secession";
+		}
+		
+		ra.addFlashAttribute("message", message);
+		return path;
+	}
+	
+	
+	// -------------------------------------------
+	
+	// MultipartFile : multipart/form-data 형식으로 제출된 파라미터 중
+	//               file 타입 데이터만 추출해 저장하는 Spring 제공 객체
 	// - 실제 파일
 	// - 파일 이름
 	// - 파일 크기
 	// - 업로드된 파일을 지정된 경로에 저장하는 메서드
-	//
+	// (추가 설정 필요!)
 	
 	/** 프로필 이미지 수정
-	 * @param profileImg : 실제 업로드된 프로필 이미지
-	 * @param loginMember
-	 * @param ra
+	 * @param profileImg : 실제 업로드된 프로필 이미지 
+	 * 						(파일 선택 여부 관련 없이 무조건 객체 생성)
+	 * @param loginMemebr
+	 * @param ra 
 	 * @return
 	 * @throws IOException 
 	 * @throws IllegalStateException 
 	 */
 	@PostMapping("profile")
-	public String profile(@RequestParam("profileImg") MultipartFile profileImg,
-						@SessionAttribute("loginMember") Member loginMember, RedirectAttributes ra) throws IllegalStateException, IOException {
+	public String profile(
+		@RequestParam("profileImg") MultipartFile profileImg,
+		@SessionAttribute("loginMember") Member loginMember, 
+		RedirectAttributes ra) throws IllegalStateException, IOException {
 		
 		// 프로필 이미지 수정 서비스 호출 후 결과 반환
-		int result = service.profile(profileImg, loginMember);
-								// 실제 이미지 파일
+		int result = service.profile(profileImg, loginMember);			
+							// 실제 이미지 파일, 세션에 저장된 회원 정보
 		
 		// 서비스 결과에 따라 응답 제어
 		String message = null;
 		
-		if(result>0) {
-			message = "프로필 이미지가 변경되었습니다.";
-		}else {
-			message = "프로필 변경 실패";
+		if(result > 0) {
+			message = "프로필 이미지가 변경 되었습니다.";
+		} else {
+			message = "프로필 변경 실패!";
 		}
 		
 		ra.addFlashAttribute("message", message);
+		
+		// 프로필 페이지로 리다이렉트
 		return "redirect:profile";
 	}
+	
+	
+	
+	
+	
 	
 	
 }
